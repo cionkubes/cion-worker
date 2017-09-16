f16553c14e5010fb2d607a13391901d2e0d50f54
Erlend Tobiassen (STUDET) <erlentob@stud.ntnu.no>
2017-09-16 17:23:47 +0200

    Integrate with docker.

---
+++ worker.py
@@ -0,0 +1,43 @@
+import asyncio
+import os
+import time
+
+import docker
+from logzero import logger
+
+from cion_interface.service import service
+from workq.worker import Orchestrator
+
+CERTS = os.path.join(os.path.expanduser('~'), '.docker', 'machine', 'machines', 'manager')
+tls_config = docker.tls.TLSConfig(
+    client_cert=(os.path.join(CERTS, 'cert.pem'), os.path.join(CERTS,'key.pem')),
+    ca_cert=os.path.join(CERTS, 'ca.pem'),
+    verify=True
+)
+
+client = docker.DockerClient(base_url='tcp://192.168.99.100:2376', tls=tls_config)
+
+
+@service.update.implement
+async def update(svc_name, image):
+    start = time.perf_counter()
+    svc = client.services.get(svc_name)
+    ret = svc.update(image=image, name=svc_name)
+    end = time.perf_counter()
+    logger.info(f"Update took {end - start} seconds.")
+    return ret
+
+
+def main():
+    addr, port = os.environ['ORCHESTRATOR_ADDRESS'].split(':')
+
+    loop = asyncio.get_event_loop()
+
+    orchestrator = Orchestrator(addr, port)
+
+    loop.run_until_complete(orchestrator.join(service))
+    loop.close()
+
+
+if __name__ == '__main__':
+    main()
